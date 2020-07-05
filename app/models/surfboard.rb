a0ff023978aa939d16b02ec7f232cd8913572c1d
Cassandra Calescu <casscalescu@gmail.com>
2020-07-05 16:00:45 +1000

    Model Validations

--- app/models/surfboard.rb
+++ app/models/surfboard.rb
@@ -1,4 +1,17 @@
 class Surfboard < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  # Validations
+  validates :brand, presence: true
+  validates :price, presence: true, numericality: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :deposit, presence: true, numericality: true
+
+  def unavailable_dates
+    bookings.pluck(:start_date, :end_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
